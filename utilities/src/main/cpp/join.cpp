aaacd78119be1ce5ea7a860cefe77948246969db
Adam Murdoch <adam@gradle.com>
2018-09-07 08:16:39 +1000

    fixed bug

--- utilities/src/main/cpp/join.cpp
+++ utilities/src/main/cpp/join.cpp
@@ -5,8 +5,7 @@
 
 UTILITIES_API std::string join(const linked_list & source) {
     std::string result;
-    // BUG: i = 1 should be i = 0
-    for (int i = 1; i < source.size(); ++i) {
+    for (int i = 0; i < source.size(); ++i) {
         if (!result.empty()) {
             result.append(" ");
         }
